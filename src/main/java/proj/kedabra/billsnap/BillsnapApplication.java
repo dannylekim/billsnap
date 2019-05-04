3fec045d3302b1f2dca702139531a3c74dabca84
Danny Lekim <dannylekim@gmail.com>
2019-05-04 10:59:12 -0400

    Initial Commit

---
+++ src/main/java/proj/kedabra/billsnap/BillsnapApplication.java
@@ -0,0 +1,13 @@
+package proj.kedabra.billsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/kedabra/billsnap/BillsnapApplicationTests.java
@@ -0,0 +1,16 @@
+package proj.kedabra.billsnap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillsnapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
