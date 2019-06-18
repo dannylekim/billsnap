d7851296f6b4c69378cdbde4ac105a01c2c228c2
Danny Le Kim <dannylekim@gmail.com>
2019-06-17 22:42:12 -0400

    feat(#zpnfg): add BillSnapExceptionHandler

---
+++ src/main/java/proj/kedabra/billsnap/exception/BillSnapException.java
@@ -0,0 +1,18 @@
+package proj.kedabra.billsnap.exception;
+
+import lombok.Getter;
+
+@Getter
+abstract class BillSnapException extends RuntimeException {
+
+    private static final long serialVersionUID = 3872812859698299907L;
+
+    private final String message;
+
+    private final Throwable ex;
+
+    BillSnapException(final String message, final Throwable ex) {
+        this.message = message;
+        this.ex = ex;
+    }
+}
