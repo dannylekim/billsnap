5603bec93673b9937e0db14fa4e3f68c38e3fd9b
Brian Kim Lim <brianjkl240@gmail.com>
2019-09-16 18:42:13 -0400

    feat(#205dhq_#1bk1rm): add getBillsByStatusAndAccount to BillRepository

--- src/main/java/proj/kedabra/billsnap/business/repository/BillRepository.java
+++ src/main/java/proj/kedabra/billsnap/business/repository/BillRepository.java
@@ -1,8 +1,15 @@
 package proj.kedabra.billsnap.business.repository;
 
+import java.util.stream.Stream;
+
 import org.springframework.data.repository.CrudRepository;
 
+import proj.kedabra.billsnap.business.entities.Account;
 import proj.kedabra.billsnap.business.entities.Bill;
+import proj.kedabra.billsnap.business.utils.enums.BillStatusEnum;
 
 public interface BillRepository extends CrudRepository<Bill, Long> {
+
+    Stream<Bill> getBillsByStatusAndAccounts_AccBill_Account(BillStatusEnum status, Account account);
+
 }
