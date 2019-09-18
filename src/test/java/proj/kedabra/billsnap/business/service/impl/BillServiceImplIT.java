2755b5157d8714a22f06bafa134bad7ab5b76c2e
Thienng97 <duc-thien@sympatico.ca>
2019-09-18 09:57:33 -0400

    feat(#205dhq_#1bk1nz): Add new endpoint in paymentController, data files for PaymentOwed, mapper and PaymentFacade
    * fix(#205eg4): add @AutoconfigureTestDatabase annotation to build-failure causing test class (#30)
    
    * feat(#205dhq_#1bk1nz): add payment Controller Integration test
    
    * feat(#205dhq_#1bk1nz): Modify PaymentsDTO and PaymentRessource from list ot Map
    
    * feat(#205dhq_#1bk1nz): change ApiModelProperty name
    
    * feat(#205dhq_#1bk1nz): add PaymentFacadeImpl (without implementation), refactor PaymentFacede name
    
    * feat(#205dhq_#1bk1nz): add swagger annotation
    
    * feat(#205dhq_#1bk1nz): Fix Typo for Resource file, Add new PaymentOwedDTO
    
    * feat(#205dhq_#1bk1nz): fix typo
    
    * feat(#205dhq_#1bk1nz): add PayementOwed Endpoint, data class, PayementFacade
    
    fix(#205eg4): add @AutoconfigureTestDatabase annotation to build-failure causing test class (#30)
    
    
    feat(#205dhq_#1bk1nz): Modify PaymentsDTO and PaymentRessource from list ot Map
    
    feat(#205dhq_#1bk1nz): change ApiModelProperty name
    
    feat(#205dhq_#1bk1nz): add PaymentFacadeImpl (without implementation), refactor PaymentFacede name
    
    feat(#205dhq_#1bk1nz): add swagger annotation
    
    feat(#205dhq_#1bk1nz): Fix Typo for Resource file, Add new PaymentOwedDTO
    
    feat(#205dhq_#1bk1nz): fix typo

--- src/test/java/proj/kedabra/billsnap/business/service/impl/BillServiceImplIT.java
+++ src/test/java/proj/kedabra/billsnap/business/service/impl/BillServiceImplIT.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,6 +34,7 @@ import proj.kedabra.billsnap.utils.SpringProfiles;
 @ActiveProfiles(SpringProfiles.TEST)
 @SpringBootTest
 @Transactional
+@AutoConfigureTestDatabase
 class BillServiceImplIT {
 
     @Autowired
