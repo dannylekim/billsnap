992d18dfe2e60de0a99c87ec512dd871a3509406
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2019-09-19 01:36:40 -0400

    feat(#205dhq_#1bk1wb): add test class for paymentFacadeimpl

--- src/test/java/proj/kedabra/billsnap/business/facade/impl/PaymentFacadeImplTest.java
+++ src/test/java/proj/kedabra/billsnap/business/facade/impl/PaymentFacadeImplTest.java
@@ -7,44 +7,82 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.mockito.Mock;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import proj.kedabra.billsnap.business.dto.PaymentsOwedDTO;
+import proj.kedabra.billsnap.business.dto.PaymentOwedDTO;
+import proj.kedabra.billsnap.business.entities.Bill;
+import proj.kedabra.billsnap.business.repository.AccountRepository;
 import proj.kedabra.billsnap.business.repository.BillRepository;
+import proj.kedabra.billsnap.business.service.impl.BillServiceImpl;
+import proj.kedabra.billsnap.fixtures.AccountEntityFixture;
 import proj.kedabra.billsnap.fixtures.BillEntityFixture;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
 
 public class PaymentFacadeImplTest {
 
     private PaymentFacadeImpl paymentFacadeImpl;
 
     @Mock
+    private AccountRepository accountRepository;
+
+    @Mock
     private BillRepository billRepository;
+
+    @Mock
+    private BillServiceImpl billServiceImpl;
 
     @Test
     @DisplayName("Should return an exception if given an email that does not exist")
     void shouldReturnExceptionIfEmailDoesNotExist() {
         //Given
         final String testEmail = "abc@123.ca";
-        final var bill = BillEntityFixture.getDefault();
-        //when(billRepository.findBillsByStatusAccount(testEmail)).thenReturn(bill);
 
-        //When/then
+        //when
+        when(accountRepository.getAccountByEmail(testEmail)).thenReturn(null);
+
+        //then
         final ResourceNotFoundException resourceNotFoundException = assertThrows(ResourceNotFoundException.class,
                 () -> paymentFacadeImpl.getAmountsOwed(testEmail));
         assertThat(resourceNotFoundException.getMessage()).isEqualTo("Account does not exist");
     }
 
     @Test
-    @DisplayName("Should return a valid PaymentOwedDTO")
-    void shouldReturnAValidPaymentOwedDTO() {
+    @DisplayName("Should return empty PaymentOwedDTO when no bills are found")
+    void shouldReturnEmptyPaymentOwedDTOWhenNoBillsAreFound() {
         //Given
         final String testEmail = "abc@123.ca";
+        final var account = AccountEntityFixture.getDefaultAccount();
 
-        //when(billRepository.findBillsByStatusAccount(testEmail)).thenReturn(null);
-
-        //When
-        PaymentsOwedDTO paymentsOwedDTO = paymentFacadeImpl.getAmountsOwed(testEmail);
+        //when
+        when(accountRepository.getAccountByEmail(testEmail)).thenReturn(account);
+//        when(billServiceImpl.getAllBillsByAccountAndStatus(account)).thenReturn(null);
 
         //Then
+        List<PaymentOwedDTO> listPaymentOwed = paymentFacadeImpl.getAmountsOwed(testEmail);
+        assertThat(listPaymentOwed.size()).isEqualTo(0);
 
+    }
+
+    @Test
+    @DisplayName("Should return ...")
+    void shouldReturn() {
+        //Given
+        final String testEmail = "abc@123.ca";
+        final var account = AccountEntityFixture.getDefaultAccount();
+        final var bill = BillEntityFixture.getDefault();
+        final Stream<Bill> billStream = Stream.of(bill);
+
+        //when
+        when(accountRepository.getAccountByEmail(testEmail)).thenReturn(account);
+//        when(billServiceImpl.getAllBillsByAccountAndStatus(account)).thenReturn(billStream);
+
+        //then
+        List<PaymentOwedDTO> listPaymentOwed = paymentFacadeImpl.getAmountsOwed(testEmail);
+        assertThat(listPaymentOwed.size()).isEqualTo(1);
+        assertThat(listPaymentOwed.get(0).email).isEqualTo(bill.creator.email);
+        assertThat(listPaymentOwed.get(0).amount).isEqualTo(bill.items.iterator().next().cost);
     }
 
 }
