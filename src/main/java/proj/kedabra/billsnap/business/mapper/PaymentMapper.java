32ef680f4848d94eca2687e9de3a3058931b909c
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2019-09-22 16:54:59 -0400

    feat(#205dhq_#1bk1wb): add mapping paymentOwed->PaymentOwedDTO

--- src/main/java/proj/kedabra/billsnap/business/mapper/PaymentMapper.java
+++ src/main/java/proj/kedabra/billsnap/business/mapper/PaymentMapper.java
@@ -4,11 +4,14 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import proj.kedabra.billsnap.business.dto.PaymentOwedDTO;
+import proj.kedabra.billsnap.business.entities.IPaymentOwed;
 import proj.kedabra.billsnap.presentation.resources.PaymentOwedResource;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PaymentMapper {
 
     PaymentOwedResource toResource(PaymentOwedDTO paymentOwedDTO);
+
+    PaymentOwedDTO toDTO(IPaymentOwed paymentOwed);
 
 }
