8148eca5313e3bd94957172154bb7d3f02273ed2
Kevinjhlin <jinghao.lin@mail.utoronto.ca>
2019-10-29 23:16:50 -0400

    feat(#2jc8b5_#2r9byb): create ErrorMessageEnum to start replacing hard coded error messages

---
+++ src/main/java/proj/kedabra/billsnap/utils/ErrorMessageEnum.java
@@ -0,0 +1,32 @@
+package proj.kedabra.billsnap.utils;
+
+import lombok.Getter;
+
+public enum ErrorMessageEnum {
+
+    //====================================================================================================
+    MULTIPLE_TIP_METHOD("Only one type of tipping is supported. Please make sure only either tip amount or tip percent is set."),
+    ACCOUNT_DOES_NOT_EXIST("Account does not exist"),
+    LIST_ACCOUNT_DOES_NOT_EXIST("One or more accounts in the list of accounts does not exist: [%s, %s]"),
+    LIST_CANNOT_CONTAIN_BILL_CREATOR("List of emails cannot contain bill creator email");
+
+
+    @Getter
+    private final String message;
+
+    ErrorMessageEnum(final String errorMessage) {
+        this.message = errorMessage;
+    }
+
+    /**
+     * Return if value is equal to the enum chosen
+     *
+     * @param errorMessage error message to check against
+     * @return true if the error message is equivalent to the object desired, false if otherwise
+     */
+    public boolean is(String errorMessage) {
+        return this.message.equals(errorMessage);
+    }
+
+
+}
