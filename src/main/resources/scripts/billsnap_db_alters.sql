67863cec4ece56c4ffffd2f1cbad73d0381685bc
Danny Le Kim <dannylekim@gmail.com>
2020-01-23 19:33:00 -0500

    feat(#205dfz_#228ryb): add database changes

--- src/main/resources/scripts/billsnap_db_alters.sql
+++ src/main/resources/scripts/billsnap_db_alters.sql
@@ -1,1 +1,4 @@
 -- this file has the needed scripts to use on the databases from version to version to make sure the code works. Simply annotate by version + the scripts needed on the db
+
+alter table bills_vs_accounts
+    add amount_paid numeric(14, 2);
