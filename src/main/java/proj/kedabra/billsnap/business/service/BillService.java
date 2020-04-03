4333ff4cf9f3dbd2400c2b72cab157b367d68e10
Brian Kim-Lim <brianjkl240@gmail.com>
2020-04-02 22:34:08 -0400

    feat(#2jc819_#5pbbr5): add verifyUserIsBillResponsible to BillService and unit tests

--- src/main/java/proj/kedabra/billsnap/business/service/BillService.java
+++ src/main/java/proj/kedabra/billsnap/business/service/BillService.java
@@ -23,6 +23,8 @@ public interface BillService {
 
     Bill getBill(Long id);
 
+    void verifyUserIsBillResponsible(Bill bill, String userEmail);
+
     Bill inviteRegisteredToBill(Bill bill, List<Account> accounts);
 
 }
