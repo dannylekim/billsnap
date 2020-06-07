f7c51129ec1e3cacc19d9735c91e95489cc19700
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-06-07 05:03:39 -0400

    feat(#yhf8z_7cc8ef): add integration test

---
+++ src/test/java/proj/kedabra/billsnap/fixtures/EditBillResourceFixture.java
@@ -0,0 +1,35 @@
+package proj.kedabra.billsnap.fixtures;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import proj.kedabra.billsnap.presentation.resources.EditBillResource;
+import proj.kedabra.billsnap.presentation.resources.ItemResource;
+
+public class EditBillResourceFixture {
+
+    private EditBillResourceFixture() {}
+
+    public static EditBillResource getDefault() {
+        final EditBillResource editBillResource = new EditBillResource();
+        editBillResource.setName("editName");
+        editBillResource.setResponsible(AccountResourceFixture.getDefault());
+        editBillResource.setCompany("editCompany");
+        editBillResource.setCategory("editCategory");
+        editBillResource.setTipPercent(BigDecimal.valueOf(25));
+
+        List<ItemResource> items = new ArrayList<>();
+        final var item1 = new ItemResource();
+        item1.setId(1013L);
+        final var item2 = new ItemResource();
+        item2.setId(null);
+        item2.setCost(BigDecimal.valueOf(75));
+        item2.setName("rock");
+        items.add(item1);
+        items.add(item2);
+        editBillResource.setItems(items);
+
+        return editBillResource;
+    }
+}
