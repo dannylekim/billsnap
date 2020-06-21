7babcf2f3993cf9688d564b96824a60dd8a2a98e
Danny Lekim <dannylekim@gmail.com>
2020-06-21 12:37:08 -0400

    Feat/#yhf8z #7cc8mh (#94)
    * feat(#yhf8z_7cc8mh): add editbill in facade
    
    * feat(#yhf8z_7cc8mh): add integration test
    
    * feat(#yhf8z_7cc8nv): fix bug and add integration test
    
    Co-authored-by: Duc-Thien Nguyen <duc-thien@sympatico.ca>

--- src/main/resources/scripts/insert_data.sql
+++ src/main/resources/scripts/insert_data.sql
@@ -1,31 +1,50 @@
 -- noinspection SqlResolveForFile
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (1001, 'nobills@inthisemail.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '5417894561', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (1001, 'nobills@inthisemail.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '5417894561',
+        current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (1000, 'test@email.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (1000, 'test@email.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789',
+        current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (1002, 'editbill@email.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (1002, 'editbill@email.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789',
+        current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (2000, 'userdetails@service.com', '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
-                    'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (2000, 'userdetails@service.com',
+        '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
+        'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp,
+        current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (3000, 'user@user.com', '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', 'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (3000, 'user@user.com', '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', 'firstName',
+        'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp,
+        null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (4000, 'paymentowed@test.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (4000, 'paymentowed@test.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789',
+        current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (5000, 'user@hasbills.com', '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
-                    'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (5000, 'user@hasbills.com',
+        '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
+        'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED', current_timestamp,
+        current_timestamp, null);
 
-INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status, created, updated, location_id)
-             VALUES (6000, 'user@withABill.com', '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
-                     'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED',
-                     current_timestamp, current_timestamp, null);
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (6000, 'user@withABill.com',
+        '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
+        'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED',
+        current_timestamp, current_timestamp, null);
 
 INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
                      created, updated, location_id)
@@ -54,6 +73,12 @@ VALUES (9000, 'user@withADeclinedBill.com',
         '$2a$04$IV55Yhr.ICvWxGm/6hj8iua3gium/Yzyg0XBE8Nb2q1BvEzG21RiK', -- bcrypted 'somepass' 4 rounds
         'firstName', 'middleName', 'lastName', 'MALE', '123456789', current_date, 'REGISTERED',
         current_timestamp, current_timestamp, null);
+
+INSERT INTO account (id, email, password, first_name, middle_name, last_name, gender, phone_number, birth_date, status,
+                     created, updated, location_id)
+VALUES (10000, 'editBill@email.com', 'notEncrypted', 'firstTest', 'middleTest', 'lastTest', 'MALE', '123456789',
+        current_date, 'REGISTERED', current_timestamp, current_timestamp, null);
+
 
 
 INSERT INTO bill (id, name, responsible, creator, status, created, updated, category, company, occurrence, tip_percent,
@@ -113,12 +138,19 @@ VALUES (2000, 'test', 5000, 5000, 'OPEN', current_timestamp, current_timestamp, 
 
 INSERT INTO bill (id, name, responsible, creator, status, created, updated, category, company, occurrence, tip_percent,
                   tip_amount, split_by, location_id, active)
-VALUES (2001, 'editbill', 1002, 1002, 'OPEN', current_timestamp, current_timestamp, null, null, 0, null, 15, 'ITEM', null,
+VALUES (2001, 'editbill', 1002, 1002, 'OPEN', current_timestamp, current_timestamp, null, null, 0, null, 15, 'ITEM',
+        null,
         1);
 
 INSERT INTO bill (id, name, responsible, creator, status, created, updated, category, company, occurrence, tip_percent,
                   tip_amount, split_by, location_id, active)
-VALUES (1101, 'test', 5000, 5000, 'IN_PROGRESS', current_timestamp, current_timestamp, null, null, 0, 15, null, 'ITEM', null,
+VALUES (1101, 'test', 5000, 5000, 'IN_PROGRESS', current_timestamp, current_timestamp, null, null, 0, 15, null, 'ITEM',
+        null,
+        1);
+
+INSERT INTO bill (id, name, responsible, creator, status, created, updated, category, company, occurrence, tip_percent,
+                  tip_amount, split_by, location_id, active)
+VALUES (1102, 'test', 10000, 10000, 'OPEN', current_timestamp, current_timestamp, null, null, 0, 15, null, 'ITEM', null,
         1);
 
 INSERT INTO bills_vs_accounts (bill_id, account_id, percentage, status)
@@ -157,6 +189,8 @@ VALUES (1100, 2000, 100, 'ACCEPTED');
 INSERT INTO bills_vs_accounts (bill_id, account_id, percentage, status)
 VALUES (1101, 5000, 100, 'ACCEPTED');
 INSERT INTO bills_vs_accounts (bill_id, account_id, percentage, status)
+VALUES (1102, 10000, 100, 'ACCEPTED');
+INSERT INTO bills_vs_accounts (bill_id, account_id, percentage, status)
 VALUES (2001, 1002, 100, 'ACCEPTED');
 
 INSERT INTO item (id, bill_id, name, cost)
@@ -185,6 +219,8 @@ INSERT INTO item (id, bill_id, name, cost)
 VALUES (1011, 1006, 'shoes', 69.00);
 INSERT INTO item (id, bill_id, name, cost)
 VALUES (1012, 2000, 'shoes', 69.00);
+INSERT INTO item (id, bill_id, name, cost)
+VALUES (1013, 1102, 'notEditedItem', 123.00);
 
 INSERT INTO items_vs_accounts (item_id, account_id, percentage)
 VALUES (1000, 2000, 100.0);
@@ -204,6 +240,7 @@ INSERT INTO items_vs_accounts (item_id, account_id, percentage)
 VALUES (1007, 5000, 100.0);
 INSERT INTO items_vs_accounts (item_id, account_id, percentage)
 VALUES (1009, 7002, 100.0);
-
 INSERT INTO items_vs_accounts (item_id, account_id, percentage)
 VALUES (1011, 8000, 100.0);
+INSERT INTO items_vs_accounts (item_id, account_id, percentage)
+VALUES (1013, 10000, 100.0);
