927a7f572a1f29fc0c7456260dec412618484860
thien-ng <duc-thien@sympatico.ca>
2020-07-24 16:29:22 -0400

    Feat/#a4kb9m #a4pgcc (#134)
    * feat(#a4kb9m_#a4pgcc): add edit account
    
    * feat(#a4kb9m_#a4pgcc): add account service test
    
    * feat(#a4kb9m_#a4pgcc): remove blank check for firstname and lastname
    
    * feat(#a4kb9m_#a4pgcc): remove unused import

--- src/main/java/proj/kedabra/billsnap/business/dto/AccountDTO.java
+++ src/main/java/proj/kedabra/billsnap/business/dto/AccountDTO.java
@@ -1,33 +1,18 @@
 package proj.kedabra.billsnap.business.dto;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import proj.kedabra.billsnap.business.model.entities.Location;
 
 @Data
-public class AccountDTO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class AccountDTO extends BaseAccountDTO implements Serializable {
 
     private Long id;
 
     private String email;
 
     private String password;
-
-    private String firstName;
-
-    private String middleName;
-
-    private String lastName;
-
-    private String gender;
-
-    private String phoneNumber;
-
-    private LocalDate birthDate;
-
-    private Location location;
 
 }
--- src/test/java/proj/kedabra/billsnap/business/service/impl/AccountServiceImplIT.java
+++ src/test/java/proj/kedabra/billsnap/business/service/impl/AccountServiceImplIT.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
@@ -20,11 +21,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
+import proj.kedabra.billsnap.business.dto.BaseAccountDTO;
 import proj.kedabra.billsnap.business.exception.ResourceNotFoundException;
 import proj.kedabra.billsnap.business.model.entities.Account;
 import proj.kedabra.billsnap.business.repository.AccountRepository;
 import proj.kedabra.billsnap.business.utils.enums.AccountStatusEnum;
+import proj.kedabra.billsnap.business.utils.enums.GenderEnum;
 import proj.kedabra.billsnap.fixtures.AccountDTOFixture;
+import proj.kedabra.billsnap.fixtures.AccountEntityFixture;
+import proj.kedabra.billsnap.fixtures.BaseAccountDTOFixture;
 import proj.kedabra.billsnap.utils.ErrorMessageEnum;
 import proj.kedabra.billsnap.utils.SpringProfiles;
 
@@ -138,6 +143,43 @@ class AccountServiceImplIT {
         //When/Then
         assertThatExceptionOfType(ResourceNotFoundException.class).isThrownBy(() -> accountService.getAccounts(accountsList))
                 .withMessage(ErrorMessageEnum.LIST_ACCOUNT_DOES_NOT_EXIST.getMessage(List.of(nonExistentEmail, secondNonExistentEmail).toString()));
+    }
+
+    @Test
+    @DisplayName("Should edit account info successfully")
+    void shouldEditAccountInfoSuccessfully() {
+        //Given
+        final String existentEmail = "test@email.com";
+        final var editAccount = BaseAccountDTOFixture.getDefault();
+
+        //When
+        final var result = accountService.edit(existentEmail, editAccount);
+
+        //Then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getEmail()).isNotNull();
+        assertThat(result.getPassword()).isNotNull();
+        assertThat(result.getFirstName()).isEqualTo(editAccount.getFirstName());
+        assertThat(result.getLastName()).isEqualTo(editAccount.getLastName());
+        assertThat(result.getMiddleName()).isEqualTo(editAccount.getMiddleName());
+        assertThat(result.getGender()).isEqualByComparingTo(GenderEnum.FEMALE);
+        assertThat(result.getPhoneNumber()).isEqualTo(editAccount.getPhoneNumber());
+        assertThat(result.getBirthDate().getYear()).isEqualTo(2000);
+        assertThat(result.getLocation().getCity()).isEqualTo(editAccount.getLocation().getCity());
+
+    }
+
+    @Test
+    @DisplayName("Should throw exception when edit account")
+    void shouldThrowExceptionWhenEditAccount() {
+        //Given
+        final String nonExistentEmail = "nonExistent@email.com";
+        final var editAccount = BaseAccountDTOFixture.getDefault();
+
+        //When/Then
+        assertThatExceptionOfType(ResourceNotFoundException.class).isThrownBy(() -> accountService.edit(nonExistentEmail, editAccount))
+                .withMessage(ErrorMessageEnum.ACCOUNT_DOES_NOT_EXIST.getMessage());
+
     }
 
 }
