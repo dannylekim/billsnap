7eadcdd3222e58dbe0be8f09603e82d40f44bbc1
Danny Le Kim <dannylekim@gmail.com>
2020-08-28 14:48:10 -0400

    fix: add money serializer to item resource

--- src/main/java/proj/kedabra/billsnap/presentation/resources/ItemResource.java
+++ src/main/java/proj/kedabra/billsnap/presentation/resources/ItemResource.java
@@ -3,8 +3,12 @@ package proj.kedabra.billsnap.presentation.resources;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import proj.kedabra.billsnap.presentation.deserializers.MoneySerializer;
 
 @Data
 public class ItemResource implements Serializable {
@@ -16,6 +20,7 @@ public class ItemResource implements Serializable {
     private String name;
 
     @Schema(description = "Cost of the item")
+    @JsonSerialize(using = MoneySerializer.class)
     private BigDecimal cost;
 
 }
